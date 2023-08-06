88aa0621563cf0abbd65935be63b529d2aa8547d
MrTraner <juanmauricio.ochoa@hotmail.com>
2023-08-05 18:27:35 -0600

    Nueva pantalla
    Se agrega nueva pantalla CounterScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world_app/presentation/screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(child: Text('Hola Mundo')),
+        body: CounterScreen(),
       ),
     );
   }
